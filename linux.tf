cde2b95e7b6eb8ece28f5d76abf07d4f5a5eb83b
Stefan Scherer <scherer_stefan@icloud.com>
2017-06-08 21:07:10 +0200

    Rename to CHANNEL

--- linux.tf
+++ linux.tf
@@ -76,7 +76,7 @@ resource "azurerm_virtual_machine" "linux" {
     }
 
     inline = [
-      "curl -sSL https://raw.githubusercontent.com/StefanScherer/docker-init/master/ubuntu/install-docker-ce.sh | VERSION=test sh"
+      "curl -sSL https://raw.githubusercontent.com/StefanScherer/docker-init/master/ubuntu/install-docker-ce.sh | CHANNEL=test sh"
       "sudo adduser ${var.admin_username} docker"
     ]
   }
